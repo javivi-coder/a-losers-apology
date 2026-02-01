8c9be4f60632b9862a2933940ca8213dc36adf84
javivi-coder <rejasvivancojaviera@gmail.com>
2026-01-31 22:10:39 -0300

    first upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, session
+import os
+
+app = Flask(__name__)
+app.secret_key = "jwe25627411"
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    if "step" not in session:
+        session["step"] = 1
+        session["no_count"] = 0
+
+    if request.method == "POST":
+
+       if request.form.get("action") == "next":
+           session["step"] += 1
+       elif request.form.get("action") == "no":
+           session["no_count"] += 1
+       elif request.form.get("action") == "yes":
+           session["step"] = 5
+    return render_template(
+        "index.html",
+        step=session["step"],
+        no_count=session["no_count"]
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
